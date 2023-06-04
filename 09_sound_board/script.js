ca309af250f844d6865126479f31ae1667aeef31
Dustin42Codam <dkrecisz@student.codam.nl>
2023-06-04 19:13:49 +0200

    Add 09 sound board

---
+++ 09_sound_board/script.js
@@ -0,0 +1,15 @@
+const sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong']
+
+sounds.forEach((sound) => {
+  const button = document.createElement('button')
+
+  button.classList.add('btn')
+
+  button.innerText = sound
+
+  button.addEventListener('click', () => {
+    document.getElementById(sound).play()
+  })
+
+  document.getElementById('buttons').appendChild(button)
+})
