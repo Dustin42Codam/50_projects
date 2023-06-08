5808eb544e449b3cbdf8ec3d10c6a4aa93165d7a
Dustin42Codam <dkrecisz@student.codam.nl>
2023-06-08 19:27:02 +0200

    Add movie app

---
+++ 17_movie_app/README.md
@@ -0,0 +1,5 @@
+# Movie App
+
+This app allows you to search for movies using the TMDB API (https://www.themoviedb.org/)
+
+Demo: http://dustbin.live/50_projects/17_movie_app
---
+++ 17_movie_app/script.js
@@ -0,0 +1,60 @@
+const API_URL =
+  'https://api.themoviedb.org/3/discover/movie?sort_by_popularity.desc&api_key=d65b00a9f19bd68dd4959a94e4de017f&page=1'
+
+const IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
+
+const SEARCH_URL =
+  'https://api.themoviedb.org/3/search/movie?api_key=d65b00a9f19bd68dd4959a94e4de017f&query="'
+
+const form = document.getElementById('form')
+const search = document.getElementById('search')
+const main = document.getElementById('main')
+
+async function getMovies(url) {
+  const res = await fetch(url)
+  const data = await res.json()
+
+  showMovies(data.results)
+}
+
+function showMovies(movies) {
+  main.innerHtml = ''
+
+  movies.forEach((movie) => {
+    const { title, poster_path, vote_average, overview } = movie
+
+    const movieEl = document.createElement('div')
+
+    movieEl.classList.add('movie')
+
+    movieEl.innerHTML = `
+			<img src="${IMG_PATH + poster_path}" alt="" />
+			
+			<div class="movie-info">
+	  			<h3>${title}</h3>
+	  			<span class="${
+            vote_average > 5 ? (vote_average > 7.5 ? 'green' : 'orange') : 'red'
+          }">${vote_average}</span>
+			</div>
+			
+			<div class="overview">
+	  			<h3>overview</h3>
+				${overview}
+			</div>
+	`
+    main.appendChild(movieEl)
+  })
+}
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault()
+
+  const searchTerm = search.value
+
+  if (searchTerm && searchTerm !== '') {
+    getMovies(SEARCH_URL + searchTerm)
+    search.value = ''
+  } else {
+    window.location.reload()
+  }
+})
